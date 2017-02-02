e61265787ff152d78d9b5340aac67c88923b42c7
James Roper <james@jazzy.id.au>
2017-02-02 14:22:44 -0700

    Get by status in UI

--- web/src/main/java/controllers/MainController.java
+++ web/src/main/java/controllers/MainController.java
@@ -2,11 +2,16 @@ package controllers;
 
 import com.example.pet.api.PetApi;
 import com.lightbend.lagom.javadsl.api.transport.NotFound;
+import org.pcollections.TreePVector;
 import play.mvc.Controller;
 import play.mvc.Result;
+import scala.Option;
 
 import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 public class MainController extends Controller {
 
@@ -21,13 +26,32 @@ public class MainController extends Controller {
         return petApi.getPetById(id).invoke().thenApply(pet ->
                 ok(views.html.pet.render(pet))
         ).exceptionally(e -> {
-            if (e instanceof NotFound) {
+            RuntimeException re = unwrap(e);
+            if (re instanceof NotFound) {
                 return notFound("Pet with id " + id + " not found!");
-            } else if (e instanceof RuntimeException) {
-                throw (RuntimeException) e;
-            } else {
-                throw new RuntimeException(e);
             }
+            throw re;
         });
+        
+    }
+
+    public CompletionStage<Result> petsByStatus(Option<String> status) {
+        if (status.isEmpty()) {
+            return CompletableFuture.completedFuture(ok(views.html.pets.render(TreePVector.empty())));
+        } else {
+            return petApi.findPetsByStatus(TreePVector.singleton(status.get())).invoke().thenApply(pets ->
+                    ok(views.html.pets.render(pets))
+            );
+        }
+    }
+
+    private RuntimeException unwrap(Throwable throwable) {
+        if (throwable instanceof CompletionException || throwable instanceof ExecutionException) {
+            return unwrap(throwable.getCause());
+        }
+        if (throwable instanceof RuntimeException) {
+            return (RuntimeException) throwable;
+        }
+        return new RuntimeException(throwable);
     }
 }
